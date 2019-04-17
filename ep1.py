fad6272383aff97a6b04b13099e83d7fb2023c63
Beatriz <beatriz@MacBook-Pro-de-Beatriz-2.local>
2019-04-17 09:41:26 -0300

    nomes e email

--- ep1.py
+++ ep1.py
@@ -1,8 +1,8 @@
 # EP 2019-1: Escape Insper
 #
 # Alunos: 
-# - aluno A: Fulano da Silva, fulanos@insper.edu.br
-# - aluno B: Sicrano de Almeida, sicranoa1@insper.edu.br
+# - aluno A: Beatriz Lourenço, beatrizcpl@al.insper.edu.br
+# - aluno B: Nicole Costa, nicolesac@al.insper.edu.br
 
 def carregar_cenarios():
     cenarios = {

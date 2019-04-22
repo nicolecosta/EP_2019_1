e02a86580ec98981f51e61a72cb0c11bc9e44e9b
Beatriz <beatriz@MacBook-Pro-de-Beatriz-2.local>
2019-04-22 09:35:24 -0300

    teletransportes

--- ep1.py
+++ ep1.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 20 12:54:13 2019
+
+@author: beatriz
+"""
 # EP 2019-1: Escape Insper
 #
 # Alunos: 
@@ -10,7 +17,7 @@ def carregar_cenarios():
             "titulo": "Saguao do perigo",
             "descricao": "Voce esta no saguao de entrada do insper",
             "opcoes": {
-                "andar professor": "Tomar o elevador para o andar do professor",
+                "predio novo":"ir para o predio novo",
                 "biblioteca": "Ir para a biblioteca"
             }
         },
@@ -41,9 +48,9 @@ def carregar_cenarios():
             "descricao":"Voce entrou no elevador",
             "opcoes":{}
         },
-        "saiu do elevador":{
+        "saida do elevador":{
             "titulo": "Caminho",
-            "descricao": "Para onde voce que ir agora?",
+            "descricao": "",
             "opcoes": {
                 "aquario": "Pegar missao impossivel de achar um aquario vazio!",
                 "fablab": "tentar colar papelao no fablab"
@@ -52,53 +59,68 @@ def carregar_cenarios():
         "aquario": {
             "titulo": "Unico aquario vazio",
             "descricao": "Voce resolveu entrar no aquario mas acabou sendo teletransportado para o fumodromo",
-            "opcoes":{
-                "Ir para o fumodromo"
-            }
+            "opcoes":{}
         },
         "fumodromo":{
             "titulo": "",
             "descricao":"Voce foi jogado no fumodromo",
             "opcoes":{
-                "ficar ai mesmo",
-                "Voltar para o saguao de entrada"
+                "fumar":"ficar ai mesmo",
+                "inicio":"Voltar para o saguao de entrada"
             }
         },
         "fablab":{
             "titulo":"", #calca do mickey (inventario), no fablab voce pode fazer uma chave?
             'descricao': "",
             "opcoes": {
-                ""
-                ""              
+                "papelao":"colar algumas coisas com super cola", #add inventario carrro de papelao
+                "3d":"tentar usar a impressora 3D"  #chave            
+            }
+        },
+        "predio novo":{
+            "titulo":"predio do toboga",
+            "descricao":"",
+            "opcoes":{
+                "elevador":"subir de elevador",
+                "escada": "ser fitness e subir de escada"
+            }
+        },
+        "escada":{
+            "titulo":"",
+            "descricao":"",
+            "opcoes": {
+                "aquario": "Pegar missao impossivel de achar um aquario vazio!",
+                "fablab": "tentar colar papelao no fablab"
             }
         }
     }
     nome_cenario_atual = "inicio"
     return cenarios, nome_cenario_atual
-'''
-import random
-r=random.randint(1,2)
-subiu="PORTA ABRINDO!!!"
-preso="Voce esta preso no elevador"
-if r==1:
-    print (subiu)
-    #colocar um if para ir para o aquario ou para outro lugar
-else:
-    print (preso)
 
-'''
+inventario={}
+m="mickey"
+c="chave"
+p="papelao"
+
+import random
+def sorte_elevador():
+    r=random.randint(1,3)
+    subiu="PORTA ABRINDO!!!"
+    preso="Voce esta preso no elevador"
+    if r==1 or r==3:
+        print (subiu)
+        #colocar um if para ir para o aquario ou para outro lugar
+    else:
+        print (preso)
 
 def main():
     print("Na hora do sufoco!")
-    print("------------------")
-    print()
+    print("------------------\n")
     print("Parecia uma boa ideia: vou só jogar um pouquinho/assistir Netflix/"
-        "embaçar em geral. Amanhã eu começo o EP. Mas isso não deu certo...")
-    print()
+        "embaçar em geral. Amanhã eu começo o EP. Mas isso não deu certo...\n")
     print("É o dia de entregar o EP e você está muuuuito atrasado! Você está "
         "na entrada do Insper, e quer procurar o professor para pedir um "
-        "adiamento do EP (boa sorte...)")
-    print()
+        "adiamento do EP (boa sorte...)\n")
 
     cenarios, nome_cenario_atual = carregar_cenarios()
 
@@ -113,21 +135,36 @@ def main():
                 print (v)
                 print()
         opcoes = cenario_atual['opcoes']
-        if len(opcoes) == 0:
-            print("Acabaram-se suas opções! Mwo mwo mwooooo...")
-            game_over = True
-        else:
-            for k,v in opcoes.items():
-                print ("{0}: {1}".format(k,v))
-            escolha = input("Para onde quer ir? ")
-
-            if escolha in opcoes:
-                nome_cenario_atual = escolha
+        for k,v in opcoes.items():
+            print ("{0}: {1}".format(k,v))
+        escolha = input("Para onde quer ir? ")
+        if escolha=="elevador":
+            '''
+            r=random.randint(1,2)
+            subiu="PORTA ABRINDO!!!"
+            preso="Voce esta preso no elevador"
+            if r==1:
+                print (subiu)
+                cenario_atual=="saida do elevador"
             else:
-                print("Sua indecisão foi sua ruína!")
-                game_over = True
+                print (preso)
+        '''
+            nome_cenario_atual="saida do elevador"
+        elif escolha=="fablab":
+            if m not in inventario:
+                print("ache a calca do mickey para poder entrar no fab lab")
+                nome_cenario_atual="saida do elevador"
+        elif escolha=="aquario":
+            print ("Voce resolveu entrar no aquario mas acabou sendo teletransportado para o fumodromo")
+            nome_cenario_atual="fumodromo"
+        elif escolha in opcoes:
+            nome_cenario_atual = escolha
+        else:
+            print("Sua indecisão foi sua ruína!")
+            game_over = True
 
     print("Você morreu!")
+
 
 
 # Programa principal.
